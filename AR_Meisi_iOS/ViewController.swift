ca0b9b0a90ed76015819c2e0036bf3f48549fa93
s0ra <s0ra_kanda@icloud.com>
2020-01-18 16:21:22 +0900

    Initial Commit

---
+++ AR_Meisi_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR_Meisi_iOS
+//
+//  Created by s0ra on 2020/01/18.
+//  Copyright © 2020 s0ra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
