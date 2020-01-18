b207298fa9568eb8f03f2e15a157dc1b60b110e3
s0ra <s0ra_kanda@icloud.com>
2020-01-19 01:39:47 +0900

    delete

--- Pods/Target Support Files/Pods-AR_Meisi_iOS-AR_Meisi_iOSUITests/Pods-AR_Meisi_iOS-AR_Meisi_iOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AR_Meisi_iOS_AR_Meisi_iOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AR_Meisi_iOS_AR_Meisi_iOSUITests
-@end
--- Pods/Target Support Files/Pods-AR_Meisi_iOSTests/Pods-AR_Meisi_iOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AR_Meisi_iOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_AR_Meisi_iOSTests
-@end
